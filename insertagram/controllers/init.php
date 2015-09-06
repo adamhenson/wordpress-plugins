4407ad56a48a28fc17add99d4226db88b3f454b9
adamhenson <adam@hensonism.com>
2015-09-05 21:50:07 -0400

    refactor.

--- insertagram/controllers/init.php
+++ insertagram/controllers/init.php
@@ -3,24 +3,33 @@ require_once( INSERTAGRAM_DIR . '/controllers/page.php' );
 
 class InsertagramInitController
 {
+
   public function __construct()
   {
+
     $this->add_hooks();
+
   }
+
   public function add_hooks()
   {
-    $page = new InsertagramPageController();
-    register_activation_hook( __FILE__, array( $page, 'install' ) );
+
+    $pageController = new InsertagramPageController();
+
+    register_activation_hook( __FILE__, array( $pageController, 'install' ) );
+
     // actions
-    add_action( 'wp_enqueue_scripts', array( $page, 'wp_enqueue_scripts' ) );
-    add_action( 'admin_enqueue_scripts', array( $page, 'admin_enqueue_scripts' ) );
-    add_action( 'wp_head', array( $page, 'wp_head' ) );
-    add_action( 'wp_footer', array( $page, 'wp_footer' ) );
-    add_action( 'admin_head', array( $page, 'wp_head' ) );
-    add_action( 'admin_footer', array( $page, 'admin_footer' ) );
-    add_shortcode( 'insertagram', array( $page, 'shortcode' ) );
+    add_action( 'wp_enqueue_scripts', array( $pageController, 'wp_enqueue_scripts' ) );
+    add_action( 'admin_enqueue_scripts', array( $pageController, 'admin_enqueue_scripts' ) );
+    add_action( 'wp_head', array( $pageController, 'wp_head' ) );
+    add_action( 'wp_footer', array( $pageController, 'wp_footer' ) );
+    add_action( 'admin_head', array( $pageController, 'wp_head' ) );
+    add_action( 'admin_footer', array( $pageController, 'admin_footer' ) );
+    add_shortcode( 'insertagram', array( $pageController, 'shortcode' ) );
     // settings & menu actions
-    add_action( 'admin_menu', array( $page, 'add_admin_menu' ) );
-    add_action( 'admin_init', array( $page, 'settings_init' ) );
+    add_action( 'admin_menu', array( $pageController, 'add_admin_menu' ) );
+    add_action( 'admin_init', array( $pageController, 'settings_init' ) );
+    
   }
+
 }
